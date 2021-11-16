e945ec259b1502829c3a8c6f2587cff800da5e04
jummi <98qkrwjdal@naver.com>
2021-11-17 03:49:31 +0900

    Initial commit

---
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -0,0 +1,32 @@
+package com.prgrms.devcourse.configures;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/assets/**"); // 이 요청들에 대해서는 spring security 필터 체인을 적용하지 않겠다
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/me").hasAnyRole("USER", "ADMINS")    // 인증 영역 설정
+                .anyRequest().permitAll()   // 익명 영역
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+        ;
+    }
+}
