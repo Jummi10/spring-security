b53eda63dcecac96d60e344ed304a743e3e6960f
jummi <98qkrwjdal@naver.com>
2021-11-20 03:22:09 +0900

    feat: 미션1 - 로그아웃, cookie 기반 자동 로그인 설정

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -30,13 +31,25 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/me").hasAnyRole("USER", "ADMINS")    // 인증 영역 설정
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN")    // 인증 영역 설정
                 .anyRequest().permitAll()   // 익명 영역
+
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/")
                 .permitAll()
+
                 .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // default
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)    // default
+                .clearAuthentication(true)  // default
+
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(300)  // 5분
         ;
     }
 }
